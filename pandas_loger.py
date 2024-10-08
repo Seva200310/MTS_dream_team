c66fd4f0ed7a558903881e1480cbf4c01628d564
Seva200310 <sevasubbotin@yandex.ru>
2024-10-08 18:25:44 +0300

    Update pandas_loger.py

---
+++ pandas_loger.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+class Pandas_loger():
+    def __init__(self):
+        self.data = pd.DataFrame(columns = ['down_x_offset','down_y_offset','left_side_distance','right_side_distance','rotation_yaw'])
+    def add_row(self,sensor_data):
+        #self.data.append(sensor_data)
+        self.data = pd.concat([self.data, pd.DataFrame(row, index=[0])], ignore_index=True)
+    def save_csv(self):
+        self.data.to_csv("route_log.csv")
+
+loger = Pandas_loger()
+row = {'down_x_offset':-90,'down_y_offset':10,'left_side_distance':22,'right_side_distance':-9,'rotation_yaw':10}
+loger.add_row(row)
+row = {'down_x_offset':-90,'down_y_offset':10,'left_side_distance':22,'right_side_distance':-9,'rotation_yaw':10}
+loger.add_row(row)
+row = {'down_x_offset':-90,'down_y_offset':10,'left_side_distance':22,'right_side_distance':-9,'rotation_yaw':10}
+loger.add_row(row)
+row = {'down_x_offset':-90,'down_y_offset':10,'left_side_distance':22,'right_side_distance':-9,'rotation_yaw':10}
+loger.add_row(row)
+#row = [90,10,22,9,10]
+loger.save_csv()
