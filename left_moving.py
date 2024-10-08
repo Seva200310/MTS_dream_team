06f5634a9beaf1a9d72514afae4bbd6d99529928
Ширяев Антон <70949517+androlga-bot@users.noreply.github.com>
2024-10-08 18:54:42 +0300

    левый обход лабиринта(не работает)

---
+++ left_moving.py
@@ -0,0 +1,53 @@
+import requests 
+import json
+from pandas_loger import Pandas_loger
+from simulator import Robot_controller
+
+token = "5035d904-c58d-4e7f-a1bd-bcaf5d6c0bbd70b4e88a-2054-47b0-a4c3-d7950f5e4da3"
+
+
+class left_move_algo():
+    def __init__(self, length_cell):
+        self.controller = Robot_controller(token)
+        self.loger = Pandas_loger()
+        self.length_cell = length_cell
+    def change_direction(self):# выбор направления следующего шага
+        location = self.controller.get_sensor_data()
+        if (location["front_distance"]>=self.length_cell):
+            return 1
+        if (location["left_side_distance"]>=self.length_cell):
+            return 2
+        if (location["right_side_distance"]>=self.length_cell):
+            return 3
+        if (location["back_distance"]>=self.length_cell):
+            return 4
+    def get_data(self):# получение данных о текущем шаге
+        location = self.controller.get_sensor_data()
+        row = {"down_x_offset":location["down_x_offset"],
+                "down_y_offset":location["down_y_offset"],
+                "left_side_distance":location["left_side_distance"],
+                "right_side_distance":location["right_side_distance"],
+                "rotation_yaw":location["rotation_yaw"]}
+        self.loger.add_row(row)
+        return 
+    def moving(self):#хождение по кругу
+        for i in range(50):
+            self.get_data()
+            if self.change_direction() == 1:
+                self.controller.move_forward()
+            if self.change_direction() == 2:
+                self.controller.turn_left()
+            if self.change_direction() == 3:
+                self.controller.turn_right()
+            if self.change_direction() == 4:
+                self.controller.move_backward()
+        self.loger.save_csv()
+
+        
+
+
+        
+left_move_algo(6).moving()
+    
+
+#response = requests.post("http://127.0.0.1:8801/api/v1/robot-python/restart?token=5035d904-c58d-4e7f-a1bd-bcaf5d6c0bbd70b4e88a-2054-47b0-a4c3-d7950f5e4da3")
