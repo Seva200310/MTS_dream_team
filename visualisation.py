cb3906c43c3d41cca9823d73b932a46954753a7e
MalyshevaAnna2020 <almalysheva10@mail.ru>
2024-10-09 13:11:41 +0300

    Update visualisation.py
    It's a class now

--- visualisation.py
+++ visualisation.py
@@ -2,29 +2,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from math import sin, cos
 
-data = pd.read_csv("data.csv", delimiter=",", decimal='.')
-x = data["down_x_offset"]
-y = data["down_y_offset"]
+class Visualisation():
 
-dist_left = data["left_side_distance"]
-dist_right = data["right_side_distance"]
+    def __init__(self, file):
 
-rot = data["rotation_yaw"]
+        self.data = pd.read_csv(file, delimiter=",", decimal='.')
+        self.x = self.data["down_x_offset"]
+        self.y = self.data["down_y_offset"]
 
-walls_left_x = []
-walls_left_y = []
-walls_right_x = []
-walls_right_y = []
+        self.dist_left = self.data["left_side_distance"]
+        self.dist_right = self.data["right_side_distance"]
 
-for i in range(len(data)):
-    walls_left_x.append(x[i] - cos(rot[i])*dist_left[i])
-    walls_right_x.append(x[i] + cos(rot[i])*dist_right[i])
-    walls_left_y.append(y[i] + sin(rot[i])*dist_left[i])
-    walls_right_y.append(y[i] - sin(rot[i])*dist_right[i])
+        self.rot = self.data["rotation_yaw"]
 
-plt.ylim(-150, 150)
-plt.xlim(-150, 150)
+    def draw(self):
+        walls_left_x = []
+        walls_left_y = []
+        walls_right_x = []
+        walls_right_y = []
 
-plt.plot(x, y, marker='o', color='r', linewidth=1.5)
-plt.plot(walls_left_x, walls_left_y, color='g', linewidth=1.5)
-plt.plot(walls_right_x, walls_right_y, color='g', linewidth=1.5)
+        for i in range(len(self.data)):
+            walls_left_x.append(self.x[i] - cos(self.rot[i])*self.dist_left[i])
+            walls_right_x.append(self.x[i] + cos(self.rot[i])*self.dist_right[i])
+            walls_left_y.append(self.y[i] + sin(self.rot[i])*self.dist_left[i])
+            walls_right_y.append(self.y[i] - sin(self.rot[i])*self.dist_right[i])
+
+        plt.ylim(-150, 150)
+        plt.xlim(-150, 150)
+
+        plt.plot(self.x, self.y, marker='o', color='r', linewidth=1.5)
+        plt.plot(walls_left_x, walls_left_y, color='g', linewidth=1.5)
+        plt.plot(walls_right_x, walls_right_y, color='g', linewidth=1.5)
