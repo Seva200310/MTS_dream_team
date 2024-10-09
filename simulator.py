942fcef28abb71bb7c226f30de6fe37c7407a80d
Seva200310 <sevasubbotin@yandex.ru>
2024-10-09 17:05:51 +0300

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -34,7 +34,7 @@ class Robot_controller():
     def restart(self):
         response = requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/restart?token={self.token}")
         return response.content
-    def move_forward(self):
+    def move_forward(self):#Движение вперед
         response = requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/forward?token={self.token}")
         moving = [0,-1]
         yaw = self.get_sensor_data()["rotation_yaw"]
@@ -42,7 +42,7 @@ class Robot_controller():
         self.position_x+=rotated_moving[0]
         self.position_y+=rotated_moving[1]
         return response.content
-    def move_backward(self):
+    def move_backward(self):#движение назад
         response = requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/backward?token={self.token}")
         moving = [0,1]
         yaw = self.get_sensor_data()["rotation_yaw"]
@@ -50,52 +50,56 @@ class Robot_controller():
         self.position_x+=rotated_moving[0]
         self.position_y+=rotated_moving[1]
         return response.content
-    def turn_left(self):
+    def turn_left(self):#Поворот налево
         response = requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/left?token={self.token}")
         return response.content
-    def turn_right(self):
+    def turn_right(self):#поворот направо
         response = requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/right?token={self.token}")
         return response.content
-    def get_sensor_data(self):
+    def get_sensor_data(self):#получить сырые данные с сенсора
         response = requests.get(f"http://127.0.0.1:8801/api/v1/robot-python/sensor-data?token={self.token}")
         return json.loads(response.content)
     def get_sensor_data_converted(self):#функция которая возвращает sensor_data относительно ячееки матрицы
         data = self.get_sensor_data()
-        data_dist = [data["front_distance"],data["right_distance"],data["back_distance"],data["left_distance"]]
+        data_dist = [data["front_distance"],data["right_side_distance"],data["back_distance"],data["left_side_distance"]]
         converted_data_dist = []
         for dist in data_dist:
             converted_data_dist.append(round((dist-5)/16.6))
+        yaw = data["rotation_yaw"]
+        current_wall_config_north,current_wall_config=self.get_current_wall_config(converted_data_dist,yaw)
+
+        offset_x = self.central_point[0]-self.position_x
+        offset_y = self.central_point[1]-self.position_y
+        
+        converted_data = {"position_x":self.position_x,"position_y":self.position_y,"offset_x":offset_x,"offset_y":offset_y,"current_wall_config":current_wall_config,"current_wall_config_north":current_wall_config_north,"yaw": yaw}
+        return converted_data
 
 
     def rotate_movement_vector(self,yaw,moving):
         # Определяем сдвиг вектора движения в зависимости от yaw
-        shift = yaw // 90
+        shift = int(yaw) // 90
 
         # Создаем список направлений движения
         directions = [moving, [-moving[1], 0], [0, -moving[1]], [moving[1], 0]]
 
         return directions[shift]
 
-    def get_current_wall_config(self):#функция возвращающая положение стен в текущей конфигурации
-        #Получаем преобразованные данные с датчиков
-        data = self.get_sensor_data_converted()
-        data_dist = [data["front_distance"],data["right_distance"],data["back_distance"],data["left_distance"]]
+    def get_current_wall_config(self,data_dist,yaw):#функция возвращающая положение стен в текущей конфигурации
         wall_config = []
         for distance in data_dist:
-            if distance == 0:#там где расстояние равно 5 стена
+            if distance == 0:#там где расстояние равно 0 стена
                 wall_config.append(1)
             else:
                 wall_config.append(0)
 
         #повораиваем данные относительно севера
-        yaw = data["rotation_yaw"]
         rotated_config = self.rotate_wall_config(wall_config, yaw)
         self.map[self.position_x][self.position_y] = rotated_config
-        return rotated_config
+        return rotated_config,wall_config#Возращаем конфигурацию стен относительно севера и относительно робота
         
     def rotate_wall_config(self,wall_config, yaw):
         # Определяем, на сколько позиций нужно сдвинуть конфигурацию стен
-        shift = yaw // 90
+        shift = int(yaw) // 90
         # Если угол поворота больше 180 градусов, сдвигаем элементы в обратном направлении
         if yaw > 180:
             shift = 4 - shift
@@ -106,8 +110,8 @@ class Robot_controller():
 
 def iter_test(controller,method):
     method()
-    response = controller.get_sensor_data()
-    return response
+    data = controller.get_sensor_data_converted()
+    return data
 
 
 
@@ -115,7 +119,7 @@ controller = Robot_controller(token)
 config =[0,0,1,0]
 yaw = 270
 print(controller.rotate_wall_config(config,yaw))
-'''
+
 controller.restart()
 
 print(controller.wall_encoding_dict[str([0,0,0,0])])
@@ -141,4 +145,6 @@ print('------------------------------------------------------')
 print('Вперед')
 res = iter_test(controller,controller.move_forward)
 print(res)
-'''
+print("Result")
+print('------------------------------------------------------')
+print(controller.map)
