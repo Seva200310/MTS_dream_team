e1f5f8d1522eccb18da7d2aed3670e130c7c9f0c
Seva200310 <sevasubbotin@yandex.ru>
2024-10-10 14:50:52 +0300

    Исправлена запись маршрута

--- simulator.py
+++ simulator.py
@@ -4,58 +4,50 @@ import json
 token = "5035d904-c58d-4e7f-a1bd-bcaf5d6c0bbd70b4e88a-2054-47b0-a4c3-d7950f5e4da3"
 
 maze_dict = {0:[1,1,1,1],1:[1,1,1,0]}#МЫШЬ СМОТРИТ НА СЕВЕР!!!!!!!!
-class Robot_controller():
+class Maze_bot():
     def __init__(self,token):
         self.token = token
         self.position_x = 0
         self.position_y = 15
         self.central_point = [7,8]
-        #словарь позволяющий закодировать расположение стен в точке
-        self.wall_encoding_dict = {
-            str([0,0,0,0]):0,
-            str([0,0,0,0]):1,
-            str([0,0,0,0]):2,
-            str([0,0,0,0]):3,
-            str([0,0,0,0]):4,
-            str([0,0,0,0]):5,
-            str([0,0,0,0]):6,
-            str([0,0,0,0]):7,
-            str([0,0,0,0]):8,
-            str([0,0,0,0]):9,
-            str([0,0,0,0]):10,
-            str([0,0,0,0]):11,
-            str([0,0,0,0]):12,
-            str([0,0,0,0]):13,
-            str([0,0,0,0]):14,
-            str([0,0,0,0]):15,
-        }
-        self.map = [[[1,1,1,1] for _ in range(16)] for _ in range(16)]#задаем изначальное состояние карты - все клетки отмечены как непосещенные 
+        
+        
+        self.map = [[[1,1,1,1] for _ in range(16)] for _ in range(16)]#задаем изначальное состояние карты - все клетки отмечены как непосещенные
+        self.route = []
 
     def restart(self):
         response = requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/restart?token={self.token}")
         return response.content
     def move_forward(self):#Движение вперед
-        response = requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/forward?token={self.token}")
-        moving = [0,-1]
-        yaw = self.get_sensor_data()["rotation_yaw"]
-        rotated_moving = self.rotate_movement_vector(yaw,moving)
-        self.position_x+=rotated_moving[0]
-        self.position_y+=rotated_moving[1]
-        return response.content
+        wall_config = self.sensor_data["current_wall_config"]
+        yaw = self.sensor_data["yaw"]
+        if wall_config[0] == 0:
+            response = requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/forward?token={self.token}")
+            moving = [0,-1]
+            rotated_moving = self.rotate_movement_vector(yaw,moving)
+            self.position_x+=rotated_moving[0]
+            self.position_y+=rotated_moving[1]
+            self.route.append([self.position_x,self.position_y])
+        self.sensor_data  = self.get_sensor_data_converted()
+        
     def move_backward(self):#движение назад
-        response = requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/backward?token={self.token}")
-        moving = [0,1]
-        yaw = self.get_sensor_data()["rotation_yaw"]
-        rotated_moving = self.rotate_movement_vector(yaw,moving)
-        self.position_x+=rotated_moving[0]
-        self.position_y+=rotated_moving[1]
-        return response.content
+        wall_config = self.sensor_data["current_wall_config"]
+        yaw = self.sensor_data["yaw"]
+        if wall_config[3] == 0:
+            requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/backward?token={self.token}")
+            moving = [0,1]
+            rotated_moving = self.rotate_movement_vector(yaw,moving)
+            self.position_x+=rotated_moving[0]
+            self.position_y+=rotated_moving[1]
+            self.route.append([self.position_x,self.position_y])
+        self.sensor_data  = self.get_sensor_data_converted()
+        
     def turn_left(self):#Поворот налево
         response = requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/left?token={self.token}")
-        return response.content
+        self.sensor_data  = self.get_sensor_data_converted()
     def turn_right(self):#поворот направо
         response = requests.post(f"http://127.0.0.1:8801/api/v1/robot-python/right?token={self.token}")
-        return response.content
+        self.sensor_data  = self.get_sensor_data_converted()
     def get_sensor_data(self):#получить сырые данные с сенсора
         response = requests.get(f"http://127.0.0.1:8801/api/v1/robot-python/sensor-data?token={self.token}")
         return json.loads(response.content)
@@ -72,6 +64,7 @@ class Robot_controller():
         offset_y = self.central_point[1]-self.position_y
         
         converted_data = {"position_x":self.position_x,"position_y":self.position_y,"offset_x":offset_x,"offset_y":offset_y,"current_wall_config":current_wall_config,"current_wall_config_north":current_wall_config_north,"yaw": yaw}
+        self.sensor_data  = converted_data
         return converted_data
 
 
@@ -116,37 +109,35 @@ def iter_test(controller,method):
     return data
 
 
+if True:
+    controller = Maze_bot(token)
+    config =[0,0,1,0]
+    yaw = 270
+    print(controller.rotate_wall_config(config,yaw))
 
-controller = Robot_controller(token)
-config =[0,0,1,0]
-yaw = 270
-print(controller.rotate_wall_config(config,yaw))
+    controller.restart()
 
-controller.restart()
-
-print(controller.wall_encoding_dict[str([0,0,0,0])])
-
-
-print('------------------------------------------------------')
-print('Начало испытания')
-res = controller.get_sensor_data_converted()
-print(res)
-print('------------------------------------------------------')
-print('Направо')
-res = iter_test(controller,controller.turn_right)
-print(res)
-print('------------------------------------------------------')
-print('Вперед')
-res = iter_test(controller,controller.move_forward)
-print(res)
-print('------------------------------------------------------')
-print('Вперед')
-res = iter_test(controller,controller.move_forward)
-print(res)
-print('------------------------------------------------------')
-print('Вперед')
-res = iter_test(controller,controller.move_forward)
-print(res)
-print("Result")
-print('------------------------------------------------------')
-#print(controller.map)
+    print('------------------------------------------------------')
+    print('Начало испытания')
+    res = controller.get_sensor_data_converted()
+    print(res)
+    print('------------------------------------------------------')
+    print('Направо')
+    res = iter_test(controller,controller.turn_right)
+    print(res)
+    print('------------------------------------------------------')
+    print('Вперед')
+    res = iter_test(controller,controller.move_forward)
+    print(res)
+    print('------------------------------------------------------')
+    print('Вперед')
+    res = iter_test(controller,controller.move_forward)
+    print(res)
+    print('------------------------------------------------------')
+    print('Вперед')
+    res = iter_test(controller,controller.move_forward)
+    print(res)
+    print("Result")
+    print('------------------------------------------------------')
+    print(controller.route)
+    #print(controller.map)
---
+++ tremaux.py
@@ -0,0 +1,19 @@
+from simulator import Maze_bot
+class Tremaux(Maze_bot):
+    def __init__(self,token):
+        super().__init__(token)
+        self.cells_visit_map = [[0 for _ in range(16)] for _ in range(16)]#задаем изначальное состояние карты - все клетки отмечены как непосещенные
+        self.token = token
+    def mark_cell(self):#Помечаем 
+        pass
+    def check_impass(self):
+        pass
+    def check_crossroad(self):
+        pass
+    def find_ways_from_cell():
+        pass
+    
+
+
+token = "5035d904-c58d-4e7f-a1bd-bcaf5d6c0bbd70b4e88a-2054-47b0-a4c3-d7950f5e4da3"
+alg = Tremaux(token)
