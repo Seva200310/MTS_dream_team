61240933563d66f8e63036a44a372c8bc868a076
MalyshevaAnna2020 <75412506+MalyshevaAnna2020@users.noreply.github.com>
2024-10-11 15:07:12 +0300

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -2,20 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from math import sin, cos
 import numpy as np
-from simulator import Robot_controller
+from simulator import Maze_bot
+from array import array
 
 class Visualisation():
 
-    def __init__(self, file, robot:Robot_controller):
+    def __init__(self, robot:Maze_bot):
+        self.robot = robot
 
-        self.data = pd.read_csv(file, delimiter=",", decimal='.')
-        self.x = self.data["down_x_offset"]
-        self.y = self.data["down_y_offset"]
-
-        self.dist_left = self.data["left_side_distance"]
-        self.dist_right = self.data["right_side_distance"]
-
-        self.rot = self.data["rotation_yaw"]
+        self.x = [i[0] for i in robot.route] * 166
+        self.y = [i[1] for i in robot.route] * 166
 
         # Размер клетки и толщина границы
         self.cell_size = 156
@@ -27,7 +23,8 @@ class Visualisation():
 
         self.map = robot.map
 
-        print(len(self.data))
+        # plt.ion()
+        plt.show(block=False)
 
     def max_data(self):
         print("max x: " + str(max(self.x)))
@@ -61,28 +58,34 @@ class Visualisation():
         # Размер изображения с учетом границ
         image_size = (self.rows * self.cell_size + (self.rows + 1) * self.border_width,
                     self.cols * self.cell_size + (self.cols + 1) * self.border_width)
-        print(image_size)
+        # print(image_size)
         # Создание изображения с белым фоном
         image = np.zeros(image_size)
 
         # Рисование границ клеток черным цветом
         for i in range(self.rows):
             for j in range(self.cols):
+                n = i
+                m = j + 1
                 if self.map[i][j][0] == 1:
-                    self.draw_up_border(i, j, image)
+                    self.draw_up_border(m, n, image)
                 if self.map[i][j][1] == 1:
-                    self.draw_right_border(i, j, image)
+                    self.draw_right_border(m, n, image)
                 if self.map[i][j][2] == 1:
-                    self.draw_down_border(i, j, image)
+                    self.draw_down_border(m, n, image)
                 if self.map[i][j][1] == 1:
-                    self.draw_left_border(i, j, image)
+                    self.draw_left_border(m, n, image)
         
         plt.imshow(image, cmap='binary', interpolation='nearest')
 
     def draw(self):
+        self.x = [(i[0] - self.robot.route[0][0]) * 166 + 83 for i in self.robot.route]
+        self.y = [(i[1] - self.robot.route[0][1] + 16) * 166 - 83 for i in self.robot.route]
         self.draw_maze()
-        plt.plot(10*(self.x - self.x[0] + 8.3), 10*(-self.y - self.y[0] + 8.3), marker='o', color='r', linewidth=1.5)
-        plt.show()
+        if len(self.x) > 0:
+            plt.plot(self.x, self.y, marker='o', color='r', linewidth=1.5)
+        # plt.draw()
+        plt.pause(0.001)
 
     def find_delta(self):
         delta_average = 0
@@ -95,10 +98,3 @@ class Visualisation():
                 print(delta)
         delta_average = delta_average / count
         print("Среднее значение передвижения робота: " + str(delta_average))
-
-# visual = Visualisation("route_log.csv")
-
-# visual.draw()
-# visual.find_delta()
-# visual.min_data()
-# visual.max_data()
